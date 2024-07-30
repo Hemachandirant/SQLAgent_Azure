b29ffd505710b8f9bfcab5bea0eab3f0c30c33d5
Hemachandiran <83321708+Hemachandirant@users.noreply.github.com>
2024-07-30 12:55:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -175,5 +175,4 @@ def query_database(request: QueryRequest):
 
     
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
